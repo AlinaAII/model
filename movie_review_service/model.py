03ad0cc33b0f5baed99b125b5fb25791da4ed872
AlinaAII <all5wrj7@gmail.com>
2024-10-17 00:46:08 +0300

    Update model.py

--- movie_review_service/model.py
+++ movie_review_service/model.py
@@ -1,15 +1,12 @@
-import joblib
-import os
-from pathlib import Path
-
 def load_model():
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    MODEL_PATH = os.path.join(BASE_DIR, 'sentiment_model.joblib')
+    MODEL_PATH = os.path.join(BASE_DIR, 'model.pkl')
     full_path = os.path.abspath(MODEL_PATH)
     print(f"Используем путь к модели: {full_path}")
 
     try:
-        model = joblib.load(full_path)
+        with open(full_path, 'rb') as f:
+            model = pickle.load(f)
         print("Модель загружена успешно.")
         return model
     except FileNotFoundError:
@@ -18,15 +15,3 @@ def load_model():
     except Exception as e:
         print(f"Произошла ошибка при загрузке модели: {str(e)}")
         raise
-
-
-try:
-    BASE_DIR = Path(__file__).resolve().parent.parent
-    MODEL_PATH = os.path.join(BASE_DIR, 'sentiment_model.joblib')
-    full_path = os.path.abspath(MODEL_PATH)
-    joblib.dump(model, MODEL_PATH)
-    print("Модель успешно сохранена.")
-except Exception as e:
-    print(f"Ошибка при сохранении модели: {str(e)}")
-    raise
-
